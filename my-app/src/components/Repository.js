ee21e788b33694a9881cbd9e4a4fdd6930bdf1f6
Rizki Candra <mhdrizkicandra@gmail.com>
2021-06-01 13:24:53 +0700

    Loading animation

--- my-app/src/components/Repository.js
+++ my-app/src/components/Repository.js
@@ -28,30 +28,33 @@ export default function Repository(){
     <>
       <div className="container mt-5">
         <div className="row">
-        <table class="table table-bordered table-hover">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>URL</th>
-              <th>Language</th>
-              <th>Login</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              repos.map((repo, number) => (
+          {
+            loading ? <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_dgBN4P.json"  background="transparent"  speed="1"  style={{width: "750px", height: "750px", margin: "auto"}}  loop  autoplay></lottie-player> :
+            <table class="table table-bordered table-hover">
+              <thead>
                 <tr>
-                  <td>{repo.id}</td>
-                  <td>{repo.name}</td>
-                  <td>{repo.html_url}</td>
-                  <td>{repo.language}</td>
-                  <td>{repo.owner.login}</td>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>URL</th>
+                  <th>Language</th>
+                  <th>Login</th>
                 </tr>
-            ))
-            }
-          </tbody>
-        </table>
+              </thead>
+              <tbody>
+                {
+                  repos.map((repo, number) => (
+                    <tr>
+                      <td>{repo.id}</td>
+                      <td>{repo.name}</td>
+                      <td>{repo.html_url}</td>
+                      <td>{repo.language}</td>
+                      <td>{repo.owner.login}</td>
+                    </tr>
+                ))
+                }
+              </tbody>
+            </table>
+          }
         </div>
       </div>
     </>
